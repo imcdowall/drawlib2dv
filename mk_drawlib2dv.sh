99dd43cd8be0c59caff17fe24a0088f32d91af9d
Ian McDowall <i.d.mcdowall@gmail.com>
2020-05-22 12:29:48 +0100

    Change output filre extensions for ease of grep

--- mk_drawlib2dv.sh
+++ mk_drawlib2dv.sh
@@ -1,6 +1,6 @@
-cat drawlib.js draw.js coords.js window.js group.js items.js items_rect.js items_ellipse.js items_text.js items_image.js items_polyline.js items_freehand.js items_polygon.js  background.js >drawlib2dv_debug.min.js
-python jsmin.py <drawlib2dv_debug.min.js >drawlib2dv1.min.js
+cat drawlib.js draw.js coords.js window.js group.js items.js items_rect.js items_ellipse.js items_text.js items_image.js items_polyline.js items_freehand.js items_polygon.js  background.js >drawlib2dv_debug.js.min
+python jsmin.py <drawlib2dv_debug.js.min >drawlib2dv1.js.min
 cat copyright.js drawlib2dv1.min.js >drawlib2dv.min.js
-rm drawlib2dv1.min.js
+rm drawlib2dv1.js.min
 python ext_comments.py >documentation.txt
 
--- mk_drawlib2dv_debug.sh
+++ mk_drawlib2dv_debug.sh
@@ -1,2 +1,2 @@
-cat drawlib.js draw.js coords.js window.js group.js items.js items_rect.js items_ellipse.js items_text.js items_image.js items_polyline.js items_freehand.js items_polygon.js  background.js >drawlib2dv.min.js
+cat drawlib.js draw.js coords.js window.js group.js items.js items_rect.js items_ellipse.js items_text.js items_image.js items_polyline.js items_freehand.js items_polygon.js  background.js >drawlib2dv.js.min
 
